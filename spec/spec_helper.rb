73f6862c9e347b26c4f79f13f420421e31a6fc77
Haridas <haridasnykiel@gmail.com>
2016-08-30 17:18:15 +0100

    Refactored code and added a test to check that the text post was created.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'rspec'
 require 'pry'
-require 'watir'
+require 'watir-webdriver'
 require 'yaml'
 # require './application'
 
--- spec/tumblr_automation_spec.rb
+++ spec/tumblr_automation_spec.rb
@@ -2,21 +2,17 @@ require 'spec_helper.rb'
 
 describe "Tumblr tests" do
 
-    before :all do
-      @driver = Watir::Browser.start("https://www.tumblr.com")
-    end
-
-    after :all do
-      @driver.close
-    end
-    # let(:driver) { Watir::Browser.start("https://www.tumblr.com")}
-    # let(:wait) { Watir::Wait.until(600)}
-
-    # ep = YAML.load(File.open("login_info.yml"))
-
-
-  it "Load YAML file, should username and password." do
+  before :all do
+    @driver = Watir::Browser.start"https://www.tumblr.com"
     @ep = YAML.load(File.open("./spec/login_info.yml"))["Account"]
+  end
+
+  after :all do
+    @driver.close
+    puts "        Your tests are successful        "
+  end
+
+  it "Load YAML file" do
     expect(@ep.to_a.size).to eq 2
     expect(@ep["email"]).to eq "haridasnykiel@gmail.com"
     expect(@ep["password"]).to eq "hello123"
@@ -24,10 +20,6 @@ describe "Tumblr tests" do
 
 
   it "Should be able to login to tumblr" do
-    @ep = YAML.load(File.open("./spec/login_info.yml"))["Account"]
-    # binding.pry
-    # driver.goto("https://www.tumblr.com")
-    # binding.pry
     b = @driver.button id: "signup_login_button"
     b.when_present.click
 
@@ -37,13 +29,12 @@ describe "Tumblr tests" do
     # binding.pry
     n = @driver.button id: "signup_forms_submit"
     n.when_present.click
-    # binding.pry
+
     p = @driver.text_field(id: "signup_password")
     p.when_present.set(@ep["password"])
-    # binding.pry
-    login = @driver.button id: "signup_forms_submit"
-    login.when_present.click
-    # binding.pry
+
+    n.when_present.click
+
     expect(@driver.url).to eq "https://www.tumblr.com/dashboard"
     expect(@driver.a class: "new_post_label").to exist
 
@@ -52,22 +43,30 @@ describe "Tumblr tests" do
   end
 
   it "Should be able to post a text post." do
-    # driver.goto("https://www.tumblr.com/dashboard")
-    # binding.pry
+
     post = @driver.a id: "new_post_label_text"
     post.when_present.click
-    # binding.pry
-    title = @driver.div class: "editor editor-plaintext"
+
+    title = @driver.div class: "editor-plaintext"
     title.when_present.send_keys "Test post"
 
-    des = @driver.div class: "editor editor-richtext"
+    des = @driver.div class: "editor-richtext"
     des.when_present.send_keys "test stuffffffffff"
 
-    sub = @driver.button class: "button-area create_post_button"
+    sub = @driver.button class: "create_post_button"
     sub.when_present.click
 
-    # expect()
     # binding.pry
+
+    post_body = @driver.div class: "post_body"
+
+    if (post_body.text == "Test post\ntest stuffffffffff")
+      expect(post_body.text).to eq "Test post\ntest stuffffffffff"
+    else
+      expect(post_body.text).to eq "test stuffffffffff"
+    end
+
+
   end
 
 
