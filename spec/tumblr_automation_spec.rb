307173563b5d1498c13e9ef031ecbc6787fb40bb
Haridas <haridasnykiel@gmail.com>
2016-09-01 16:00:32 +0100

    Refactored code and created and passed tests for logging out.

--- spec/tumblr_automation_spec.rb
+++ spec/tumblr_automation_spec.rb
@@ -8,7 +8,7 @@ describe "Tumblr tests" do
   end
 
   after :all do
-    @driver.close
+    # @driver.close
     puts "        Your tests are successful        "
   end
 
@@ -22,52 +22,44 @@ describe "Tumblr tests" do
   it "Should be able to login to tumblr" do
     b = @driver.button id: "signup_login_button"
     b.when_present.click
-
-
     e = @driver.text_field(id: "signup_determine_email")
     e.when_present.set(@ep["email"])
-    # binding.pry
     n = @driver.button id: "signup_forms_submit"
     n.when_present.click
-
     p = @driver.text_field(id: "signup_password")
     p.when_present.set(@ep["password"])
-
     n.when_present.click
-
     expect(@driver.url).to eq "https://www.tumblr.com/dashboard"
     expect(@driver.a class: "new_post_label").to exist
-
-    # driver.close
-
   end
 
   it "Should be able to post a text post." do
-
     post = @driver.a id: "new_post_label_text"
     post.when_present.click
-
     title = @driver.div class: "editor-plaintext"
     title.when_present.send_keys "Test post"
-
     des = @driver.div class: "editor-richtext"
     des.when_present.send_keys "test stuffffffffff"
-
     sub = @driver.button class: "create_post_button"
     sub.when_present.click
-
-    # binding.pry
-
     post_body = @driver.div class: "post_body"
-
     if (post_body.text == "Test post\ntest stuffffffffff")
       expect(post_body.text).to eq "Test post\ntest stuffffffffff"
     else
       expect(post_body.text).to eq "test stuffffffffff"
     end
-
-
   end
+
+  it "Should be able to logout" do
+    drop = @driver.div(id: "account_button").button(class: "tab_anchor")
+    drop.when_present.click
+    logout_link = @driver.a(id: "logout_button")
+    logout_link.click
+    ok_button = @driver.button(class: "ui_button btn_1 chrome blue")
+    ok_button.click
+    expect(@driver.url).to eq "https://www.tumblr.com/login"
+  end
+
 
 
 end
